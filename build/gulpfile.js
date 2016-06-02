aa187eec337ccf6c5c60797cf4a28f7f1bf0a788
umissthestars@gmail.com <umissthestars@gmail.com>
2016-06-02 15:39:26 +0800

    Updata the package.json
    Updata the package.json

--- build/gulpfile.js
+++ build/gulpfile.js
@@ -1,8 +1,6 @@
 var fs = require('fs');
 
 var gulp = require('gulp');
-var react = require('gulp-react');
-var babel = require('gulp-babel');
 var webpack = require('webpack'); 
 
 var config = Object.create(require('./webpack.config.js'));
